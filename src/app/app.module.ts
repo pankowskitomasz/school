0c54a514f237b6eb138e95b8711181794542b5da
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-12-06 13:29:20 +0100

    project's initial configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+
 import { AppComponent } from './app.component';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComingsoonComponent,
+    ErrorComponent,
+    FooterComponent,
+    NavbarComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
