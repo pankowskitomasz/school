0ec789fcc5d726dd893b1e9d4e4fbf25574add53
pankowskitomasz <pankowski.email@gmail.com>
2021-10-18 16:27:37 +0200

    layout: contact,login,register

--- src/app/comingsoon/comingsoon.component.ts
+++ src/app/comingsoon/comingsoon.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comingsoon',
   templateUrl: './comingsoon.component.html',
-  styleUrls: ['./comingsoon.component.css']
+  styleUrls: ['../sass/styles.scss']
 })
 export class ComingsoonComponent implements OnInit {
 
